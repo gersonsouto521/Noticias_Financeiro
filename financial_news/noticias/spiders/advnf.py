868436197e4cd6fb334d21bfff0864b4a5940dce
gersonsouto <gersonsouto94@gmail.com>
2020-03-18 14:26:04 -0300

    spider capturando title/data/image/href dos cards das noticias

--- financial_news/noticias/spiders/advnf.py
+++ financial_news/noticias/spiders/advnf.py
@@ -4,7 +4,20 @@ import scrapy
 
 class AdvnfSpider(scrapy.Spider):
     name = 'advnf'
-    start_urls = ['http://http://br.advfn.com/jornal/acoes/balanco-trimestral/']
+    start_urls = ['https://br.advfn.com/jornal/acoes/balanco-trimestral/']
 
     def parse(self, response):
-        pass
+        articles = response.xpath('/html/body/div[4]/div[4]/div/div/div[1]/article')
+
+        for article in articles:
+            link = article.xpath('.')
+            title = link.xpath('.//div/div/a/text()').extract_first()
+            href = link.xpath('.//div/div/a/@href').extract_first()
+            data = link.xpath('.//div/div/div[1]/time/text()').extract_first()
+            image = link.xpath('.//div/a/img/@src').extract_first()
+            yield{
+                'title':title,
+                'href':href,
+                'data':data,
+                'image':image,
+            }
